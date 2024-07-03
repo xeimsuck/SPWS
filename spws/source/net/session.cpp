87a73bd15391598f6ae4f90d7ed48f3ea7d832a3
xeim <xeimdonot@gmail.com>
2024-07-03 20:34:41 +0000

    created session class

---
+++ spws/source/net/session.cpp
@@ -0,0 +1,42 @@
+#include "session.hpp"
+#include <iostream>
+
+spws::net::session::session(boost::asio::ip::tcp::socket &&socket) :
+                    socket(std::move(socket)) {
+}
+
+void spws::net::session::run() {
+    std::cout << "run\n";
+    listen();
+}
+
+void spws::net::session::listen() {
+    auto self(shared_from_this());
+    boost::asio::async_read_until(socket, buffer, "\0", [self, this](const boost::system::error_code ec, std::size_t size){
+        if(error_handler(ec)) return;
+        request(size);
+        listen();
+    });
+}
+
+void spws::net::session::request(const std::size_t size) {
+    std::string str = "HTTP/1.1 200 OK\nContent-Type: text/html\r\nContent-Length: 52\r\n\r\n<html><head></head><body><h1>SPWS</h1></body></html>";
+    boost::asio::write(socket, boost::asio::buffer(str));
+    buffer.consume(buffer.size());
+}
+
+int spws::net::session::error_handler(boost::system::error_code error) {
+    if(!error) return 0;
+    switch (error.value()) {
+        case boost::asio::error::eof:
+            return 1;
+        case boost::asio::error::connection_aborted:
+            return 1;
+        case boost::asio::error::connection_reset:
+            return 1;
+        case boost::asio::error::connection_refused:
+            return 1;
+    }
+    std::cerr << "SESSION ERROR: " << error.what() << "\n";
+    return 1;
+}
---
+++ spws/source/net/session.hpp
@@ -0,0 +1,26 @@
+#ifndef SPWS_NET_SESSION_HPP
+#define SPWS_NET_SESSION_HPP
+
+#include <memory>
+#include <boost/asio.hpp>
+
+namespace spws {
+    namespace net {
+        class session : public std::enable_shared_from_this<session> {
+        public:
+            explicit session(boost::asio::ip::tcp::socket &&socket);
+
+        public:
+            void run();
+        private:
+            void listen();
+            void request(std::size_t size);
+            int error_handler(boost::system::error_code error);
+        private:
+            boost::asio::ip::tcp::socket socket;
+            boost::asio::streambuf buffer;
+        };
+    }
+}
+
+#endif //SPWS_NET_SESSION_HPP
