4015206598b7774a667ae7766d6f3005a5394ce4
xeim <xeimdonot@gmail.com>
2024-07-03 20:44:14 +0000

    created server class

---
+++ spws/source/net/server.cpp
@@ -0,0 +1,41 @@
+#include "server.hpp"
+
+#include <iostream>
+#include <memory>
+#include "session.hpp"
+
+using namespace boost::asio;
+using ip::tcp;
+
+spws::net::server::server(boost::asio::io_service &_service, ushort _port) :
+                service(_service), acceptor(_service, tcp::endpoint(tcp::v4(), _port)){
+}
+
+void spws::net::server::accept() {
+    acceptor.async_accept([this](const boost::system::error_code& error, tcp::socket socket){
+        if(error_handler(error)) return;
+        accept_handler(error, socket);
+        accept();
+    });
+}
+
+int spws::net::server::error_handler(boost::system::error_code error) {
+    if(!error) return 0;
+    switch (error.value()) {
+        case boost::asio::error::eof:
+            return 1;
+        case boost::asio::error::connection_aborted:
+            return 1;
+        case boost::asio::error::connection_reset:
+            return 1;
+        case boost::asio::error::connection_refused:
+            return 1;
+    }
+    std::cerr << "SERVER ERROR: " << error.what() << "\n";
+    return 1;
+}
+
+void spws::net::server::accept_handler(const boost::system::error_code &error, boost::asio::ip::tcp::socket &socket) {
+    if(error) throw std::runtime_error(error.what());
+    std::make_shared<session>(std::move(socket))->run();
+}
---
+++ spws/source/net/server.hpp
@@ -0,0 +1,25 @@
+#ifndef SPWS_NET_SERVER_HPP
+#define SPWS_NET_SERVER_HPP
+
+#include <boost/asio.hpp>
+
+namespace spws {
+    namespace net {
+        class server {
+        public:
+            server(boost::asio::io_service& service, ushort port);
+
+        public:
+            void accept();
+        private:
+            int error_handler(boost::system::error_code error);
+            void accept_handler(const boost::system::error_code& error, boost::asio::ip::tcp::socket& socket);
+
+        private:
+            boost::asio::io_service& service;
+            boost::asio::ip::tcp::acceptor acceptor;
+        };
+    }
+}
+
+#endif //SPWS_NET_SERVER_HPP
