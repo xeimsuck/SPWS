438a79b0b7b3bce5612efd93ec34e1926ac8d57d
xeim <xeimdonot@gmail.com>
2024-07-04 20:08:27 +0000

    added target class into server

--- spws/source/config/body.hpp
+++ spws/source/config/body.hpp
@@ -2,12 +2,17 @@
 #define SPWS_CONFIG_BODY_HPP
 
 #include <vector>
+#include <unordered_map>
 
 namespace spws {
     namespace config {
         struct body {
             struct server {
+                struct target {
+                    std::string root = "/";
+                };
                 unsigned short port = 80;
+                std::unordered_map<std::string, target> targets;
             };
             std::vector<server> servers;
         };
